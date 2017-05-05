daeb39da459d0cb5e020237327d2aaa09b3ef18c
apriljoi <aprilwoulard@gmail.com>
2017-05-05 14:48:44 -0400

    initial commit

---
+++ JollySpots/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace JollySpots.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+
+    }
+}
---
+++ JollySpots/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace JollySpots.Models
+{
+    public class Review       
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Destination { get; set; }
+        public string DateVisited { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
