d108388693191db3f3c67797be4c49284427ef2d
apriljoi <aprilwoulard@gmail.com>
2017-05-08 00:57:00 -0400

    category view updated, started styling home page

--- JollySpots/Controllers/HomeController.cs
+++ JollySpots/Controllers/HomeController.cs
@@ -26,5 +26,15 @@ namespace JollySpots.Controllers
 
             return View();
         }
+
+        public ActionResult Reviews
+        {
+            get
+            {
+                ViewBag.Message = "Jolly Spots Review Page";
+
+                return View();
+            }
+        }
     }
 }
